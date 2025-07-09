7df02363e78c4af4517246e4e66fa6b7c6c39423
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-09 13:35:10 +0400

    lists with python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,7 +6,7 @@ def list_all(mongo_collection):
     """
     this is function
     """
-    if mongo_collention is None:
+    if mongo_collection is None:
         return []
     documents = list(mongo_collection.find())
     return documents
