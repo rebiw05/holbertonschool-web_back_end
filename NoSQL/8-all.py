0a2ee588a9415121b00533272044b8c27ecbdfa6
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-09 13:34:06 +0400

    lists with python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this document show how to lists
+"""
+def list_all(mongo_collection):
+    """
+    this is function
+    """
+    if mongo_collention is None:
+        return []
+    documents = list(mongo_collection.find())
+    return documents
